f5d412b9209b8fd4bee7a84aa8e32628ef0713aa
Emil Gotsev <egotsev@gmail.com>
2018-11-22 10:21:15 +0200

    postfix 11b - abs operation:

--- 11b/2018-11-22-postfix/calculator.cc
+++ 11b/2018-11-22-postfix/calculator.cc
@@ -1,11 +1,18 @@
 #include "calculator.hh"
 #include <iostream>
 #include <sstream>
+#include <exception>
 
 using namespace std;
 
 void Calculator::push(double value) {
 	values_.push_back(value);
+}
+
+double Calculator::pop() {
+	double result = values_.back();
+	values_.pop_back();
+	return result;
 }
 
 void Calculator::run(istream& in, ostream& out) {
@@ -18,6 +25,16 @@ void Calculator::run(istream& in, ostream& out) {
 		iss >> value;
 		if (!iss.fail() && iss.eof()) {
 			push(value);
+		} else {
+			double result;
+			if (token == "abs") {
+				result = pop();
+				result = result < 0 ? -result : result;
+				push(result);
+			} else {
+				throw exception();
+			}
+			out << result << endl;
 		}
 		
 	}
